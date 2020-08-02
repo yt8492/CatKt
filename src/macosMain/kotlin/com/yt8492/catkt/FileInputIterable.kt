a84a7af70a4fab179b83a8a1d16ff70355cf953d
yt8492 <tmymyut2000@gmail.com>
2020-08-02 23:40:42 +0900

    use fgetws

--- src/macosMain/kotlin/com/yt8492/catkt/FileInputIterable.kt
+++
@@ -1,9 +0,0 @@
-package com.yt8492.catkt
-
-class FileInputIterable(
-    private val filePath: String
-) : Iterable<Int> {
-    override fun iterator(): Iterator<Int> {
-        return FileInputIterator(filePath)
-    }
-}
--- src/macosMain/kotlin/com/yt8492/catkt/Main.kt
+++ src/macosMain/kotlin/com/yt8492/catkt/Main.kt
@@ -1,9 +1,16 @@
 package com.yt8492.catkt
 
-import kotlinx.cinterop.toKString
+import kotlinx.cinterop.IntVar
+import kotlinx.cinterop.allocArray
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.toKStringFromUtf32
 import kotlinx.cli.ArgParser
 import kotlinx.cli.ArgType
 import kotlinx.cli.vararg
+import platform.posix.fclose
+import platform.posix.fgetws
+import platform.posix.fopen
+import platform.posix.perror
 
 fun main(args: Array<String>) {
     val parser = ArgParser("CatKt")
@@ -11,14 +18,26 @@ fun main(args: Array<String>) {
     parser.parse(args)
     val stringBuilder = StringBuilder()
     filePaths.forEach { filePath ->
-        val fileInputIterable = FileInputIterable(filePath)
-        fileInputIterable.map {
-            it.toByte()
-        }.toByteArray()
-            .toKString()
-            .let {
-                stringBuilder.appendln(it)
+        val file = fopen(filePath, "r")
+        if (file == null) {
+            perror("cannot open input file $filePath")
+            return
+        }
+        try {
+            memScoped {
+                val bufferLength = 64 * 1024
+                val buffer = allocArray<IntVar>(bufferLength)
+                while (true) {
+                    val nextLine = fgetws(buffer, bufferLength, file)?.toKStringFromUtf32()
+                    if (nextLine.isNullOrEmpty()) {
+                        break
+                    }
+                    stringBuilder.append(nextLine)
+                }
             }
+        } finally {
+            fclose(file)
+        }
     }
-    println(stringBuilder.toString())
+    print(stringBuilder.toString())
 }
